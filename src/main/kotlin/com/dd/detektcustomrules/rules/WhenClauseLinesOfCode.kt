c28045ed8ce3d44edd5a72c147c179a94239c056
khbui <khbui@deloitte.com.au>
2022-01-24 12:00:32 +1100

    Completed when max line rule & improved test coverage

---
+++ src/main/kotlin/com/dd/detektcustomrules/rules/WhenClauseLinesOfCode.kt
@@ -0,0 +1,38 @@
+package com.dd.detektcustomrules.rules
+
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
+import org.jetbrains.kotlin.psi.KtWhenEntry
+import org.jetbrains.kotlin.psi.KtWhenExpression
+import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
+
+class WhenClauseLinesOfCode(config: Config) : Rule(config) {
+    override val issue = Issue(
+        javaClass.simpleName,
+        Severity.CodeSmell,
+        "When clause has too many lines of code until next clause",
+        Debt.FIVE_MINS,
+    )
+
+    override fun visitWhenEntry(entry: KtWhenEntry) {
+        super.visitWhenEntry(entry)
+
+        val newLines = getNewLineSizeOfWhenEntry(entry)
+
+        if (newLines > 5) {
+            report(CodeSmell(issue, Entity.from(entry),
+                "When entry contains " + newLines +
+                    "of lines of code until next entry. " +
+                    "Please reduce to 5 lines."))
+            return
+        }
+    }
+
+
+    private fun getNewLineSizeOfWhenEntry(entry : KtWhenEntry) : Int {
+        // Filter new line size does not count end of text i.e. the last new line
+        return entry.text.filter{it == '\n'}.count() + 1
+    }
+
+}
---
+++ src/test/kotlin/com/dd/detektcustomrules/rules/WhenClauseLinesOfCodeTest.kt
@@ -0,0 +1,134 @@
+package com.dd.detektcustomrules.rules
+
+import com.google.common.truth.Truth.assertThat
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.test.compileAndLint
+import org.junit.Test
+
+class WhenClauseLinesOfCodeTest {
+
+    @Test
+    fun `Should report when clause has too many lines`() {
+        val code = """
+        class A {
+            fun foo() {
+                val x = 2
+                when (x) {
+                    0 -> {
+                        methodCall(0)
+                        methodCall(1)
+                        methodCall(2)
+                        methodCall(3)
+                    }
+                    1 -> return
+                }
+            }
+
+            private fun methodCall (num : Int) {
+                print(num)
+            }
+        }
+        """
+
+        val findings = WhenClauseLinesOfCode(Config.empty).compileAndLint(code)
+        assertThat(findings).hasSize(1)
+    }
+
+    @Test
+    fun `Should report when clause has too many lines (3 violations)`() {
+        val code = """
+        class A {
+            fun foo() {
+                val x = 5
+                when (x) {
+                    0 -> {
+                        methodCall(0)
+                        methodCall(1)
+                        methodCall(2)
+                        methodCall(3)
+                    } 
+                    1 -> methodCall(24)
+                    2 -> {
+                        methodCall(4)
+                        methodCall(5)
+                        methodCall(6)
+                        methodCall(7)
+                    }
+                    3 -> methodCall(42)
+                    4 -> {
+                        methodCall(8)
+                        methodCall(9)
+                        methodCall(10)
+                        methodCall(11)
+                    }
+                }
+            }
+
+            private fun methodCall (num : Int) {
+                print(num)
+            }
+        }
+        """
+
+        val findings = WhenClauseLinesOfCode(Config.empty).compileAndLint(code)
+        assertThat(findings).hasSize(3)
+    }
+
+    @Test
+    fun `Should not report when clause has too many lines`() {
+        val code = """
+        class A {
+            fun foo() {
+                val x = 3
+                when (x) {
+                    0 -> methodCall(9)
+                    1 -> methodCall(24)
+                    2 -> return
+                }
+            }
+
+            private fun methodCall (num : Int) {
+                print(num)
+            }
+        }
+        """
+
+        val findings = WhenClauseLinesOfCode(Config.empty).compileAndLint(code)
+        assertThat(findings).hasSize(0)
+    }
+
+    @Test
+    fun `Should not report when clause has too many lines (5 lines)`() {
+        val code = """
+        class A {
+            fun foo() {
+                val x = 3
+                when (x) {
+                    0 -> {
+                        methodCall(0)
+                        methodCall(1)
+                        methodCall(2)
+                    }
+                    1 -> {
+                        methodCall(3)
+                        methodCall(4)
+                    } 
+                    2 -> {
+                        methodCall(5)
+                        methodCall(6)
+                        methodCall(7)
+                    } 
+                }
+            }
+
+            private fun methodCall (num : Int) {
+                print(num)
+            }
+        }
+        """
+
+        val findings = WhenClauseLinesOfCode(Config.empty).compileAndLint(code)
+        assertThat(findings).hasSize(0)
+    }
+
+}
