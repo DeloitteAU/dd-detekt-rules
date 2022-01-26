86e9504197769a636320e8da70f93b51cc5b3dd8
khbui <khbui@deloitte.com.au>
2022-01-27 10:15:01 +1100

    added code to rule

--- src/main/kotlin/com/dd/detektcustomrules/rules/NestedWhenStatement.kt
+++ src/main/kotlin/com/dd/detektcustomrules/rules/NestedWhenStatement.kt
@@ -1,4 +1,18 @@
 package com.dd.detektcustomrules.rules
 
-class NestedWhenStatement {
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.psi.KtWhenExpression
+
+class NestedWhenStatement(config: Config) : Rule(config) {
+    override val issue = Issue(
+        javaClass.simpleName,
+        Severity.CodeSmell,
+        "When statements should not be nested",
+        Debt.FIVE_MINS,
+    )
+
+    override fun visitWhenExpression(expression: KtWhenExpression) {
+        super.visitWhenExpression(expression)
+
+    }
 }
