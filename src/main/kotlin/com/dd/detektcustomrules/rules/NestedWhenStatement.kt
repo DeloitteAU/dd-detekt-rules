e1b5ccbf420720c1a25e0c78046bd9741baff04a
khbui <khbui@deloitte.com.au>
2022-01-27 10:53:29 +1100

    draft implementations of rule completed

--- src/main/kotlin/com/dd/detektcustomrules/rules/NestedWhenStatement.kt
+++ src/main/kotlin/com/dd/detektcustomrules/rules/NestedWhenStatement.kt
@@ -1,7 +1,11 @@
 package com.dd.detektcustomrules.rules
 
 import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.psi.KtBlockExpression
 import org.jetbrains.kotlin.psi.KtWhenExpression
+import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
+import org.jetbrains.kotlin.psi.psiUtil.findDescendantOfType
+import org.jetbrains.kotlin.psi.psiUtil.forEachDescendantOfType
 
 class NestedWhenStatement(config: Config) : Rule(config) {
     override val issue = Issue(
@@ -13,6 +17,28 @@ class NestedWhenStatement(config: Config) : Rule(config) {
 
     override fun visitWhenExpression(expression: KtWhenExpression) {
         super.visitWhenExpression(expression)
+
+//        val nested = expression.collectDescendantsOfType<KtWhenExpression>()
+//        for (st in nested) {
+//            report(
+//                CodeSmell(
+//                    issue,
+//                    Entity.from(st),
+//                    "Hello"
+//                )
+//            )
+//        }
+
+        expression.forEachDescendantOfType<KtWhenExpression> {x ->
+            report(
+                CodeSmell(
+                    issue,
+                    Entity.from(x),
+                    "Nested when statements detected. " +
+                            "Please move nested when statement to a new function."
+                )
+            )
+        }
 
     }
 }
